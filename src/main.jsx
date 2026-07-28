96128a0732c72218f6e69b9c7e98a1ef4d913255
syncworksai <syncworks.ai@gmail.com>
2026-07-28 11:58:08 -0500

    Route PM property creation separately

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 import App from "./App";
+import PMPropertyCreate from "./pages/PMPropertyCreate";
 import "./index.css";
+
+function RoutedApplication() {
+  const location = useLocation();
+
+  if (location.pathname.replace(/\/+$/, "") === "/pm/properties/new") {
+    return (
+      <ProtectedRoute>
+        <PMPropertyCreate />
+      </ProtectedRoute>
+    );
+  }
+
+  return <App />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <RoutedApplication />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
