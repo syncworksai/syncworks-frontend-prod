d55fe02ed2d1130ad251896b67f19aaa75785749
syncworksai <syncworks.ai@gmail.com>
2026-08-13 12:06:01 -0500

    Add Customer Settings connections wrapper

---
+++ src/pages/CustomerSettings.js
@@ -0,0 +1,6 @@
+import React from "react";
+import LegacyCustomerSettings from "./CustomerSettings.jsx";
+
+export default function CustomerSettingsWithConnections() {
+  return React.createElement(LegacyCustomerSettings);
+}
