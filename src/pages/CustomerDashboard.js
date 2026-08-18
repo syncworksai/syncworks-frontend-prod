a2cd9c543db36bf2294705dc333d6da9b6c94f70
syncworksai <syncworks.ai@gmail.com>
2026-08-18 15:21:05 -0500

    Route Personal home to SYNC Assistant daily experience

---
+++ src/pages/CustomerDashboard.js
@@ -0,0 +1,6 @@
+import React from "react";
+import CustomerDashboardV2 from "./CustomerDashboardV2.jsx";
+
+export default function CustomerDashboardRouter() {
+  return React.createElement(CustomerDashboardV2);
+}
