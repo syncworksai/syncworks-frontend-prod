41ab7c1aaee8e8c8f3f2afbb697d0360dbddb9c0
syncworksai <syncworks.ai@gmail.com>
2026-09-18 00:26:45 -0500

    Hide mobile navigation on player invite

--- src/components/navigation/RoleAwareMobileNav.jsx
+++ src/components/navigation/RoleAwareMobileNav.jsx
@@ -172,7 +172,7 @@ export default function RoleAwareMobileNav() {
   const hidden = useMemo(() => {
     if (!navMode) return true;
     if (/^\/tickets\/[^/]+\/?$/.test(pathname)) return true;
-    return ["/login", "/register", "/upgrade", "/gamecast", "/sports/invite", "/social/invite"].some((prefix) => pathname.startsWith(prefix));
+    return ["/login", "/register", "/upgrade", "/gamecast", "/sports/invite", "/sports/team-invite", "/social/invite"].some((prefix) => pathname.startsWith(prefix));
   }, [navMode, pathname]);
 
   useEffect(() => {
